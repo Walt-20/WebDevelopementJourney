aa3ccac4fccb68310cda5360d07bcc59c8d20b08
Walt <walt.c0des20@gmail.com>
2023-11-09 16:02:18 -0700

    creating toolbar

--- ecommerce-app/src/app/header/header.component.ts
+++ ecommerce-app/src/app/header/header.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatMenuModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
