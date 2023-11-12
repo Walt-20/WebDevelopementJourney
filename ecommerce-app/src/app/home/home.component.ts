178488aca131cddaf245f73a8d4a9043cb00ec56
Walt <walt.c0des20@gmail.com>
2023-11-11 18:55:44 -0700

    added first product

--- ecommerce-app/src/app/home/home.component.ts
+++ ecommerce-app/src/app/home/home.component.ts
@@ -3,7 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { ProductsHeaderComponent } from './components/products-header/products-header.component';
 import { FiltersComponent } from './components/filters/filters.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { ProductInformationComponent } from './components/product-information/product-information.component';
 
+
+const ROWS_HEIGHT: { [id:number]: number } = { 1: 400, 3: 335, 4: 350 }
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -11,13 +15,16 @@ import { FiltersComponent } from './components/filters/filters.component';
     CommonModule,
     MatSidenavModule,
     ProductsHeaderComponent,
-    FiltersComponent
+    FiltersComponent,
+    MatGridListModule,
+    ProductInformationComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
   category: string | undefined
 
   onColumnsCountChange(colsNum: number): void {
