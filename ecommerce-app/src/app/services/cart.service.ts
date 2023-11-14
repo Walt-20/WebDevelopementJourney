058f4efbd68e118f013265873dbd75b99a3d3982
Walt <walt.c0des20@gmail.com>
2023-11-13 19:00:07 -0700

    added functionality to add and remove buttons within cart

--- ecommerce-app/src/app/services/cart.service.ts
+++ ecommerce-app/src/app/services/cart.service.ts
@@ -26,6 +26,21 @@ export class CartService {
     console.log(this.cart.value);
   }
 
+  removeQuantity(item: CartItem): void {
+    const items = [...this.cart.value.items];
+
+    const itemInCart = items.find((_item) => _item.id === item.id);
+    if (itemInCart) {
+      itemInCart.quantity -= 1;
+    } else {
+      items.push(item);
+    }
+
+    this.cart.next({ items });
+    this._snackBar.open('1 item added to cart.', 'Ok', { duration: 3000 });
+    console.log(this.cart.value);
+  }
+
   getTotal(items: Array<CartItem>): number {
     return items.map((item) => item.price * item.quantity).reduce((prev, current) => prev + current, 0);
   }
