1fbac0375a8af923271c3f98bce0ebbfe7127d7a
Walt-20 <walt.c0des20@gmail.com>
2023-12-08 08:26:33 -0700

    creating database

--- ecommerce-app/register/server.js
+++ ecommerce-app/register/server.js
@@ -4,10 +4,16 @@ dotenv.config()
 
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
+    user: 'admin',
+    port: '3306',
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
 }).promise()
 
-const res = await pool.query('SELECT * FROM User');
-console.log(res);
+async function getUsers() {
+    const res = await pool.query('SELECT * FROM User');
+    console.log(res);
+}
+
+const users = await getUsers();
+console.log(users);
